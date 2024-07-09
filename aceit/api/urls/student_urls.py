c686891919892022fda4b8012e29c843f6ccd0cb
Mally13 <mallymutuku13@gmail.com>
2024-07-09 19:25:02 +0300

    Dump file

--- aceit/api/urls/student_urls.py
+++ aceit/api/urls/student_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from ..views import SubmitTestView, AttemptedTestsView, AttemptedTestView
 
 
-url_patterns = [
+urlpatterns = [
     path('student/tests/attempted', AttemptedTestsView.as_view(),
          name='student-attempted-tests'),
     path('student/tests/attempted/<int:test_id>', AttemptedTestView.as_view(),
